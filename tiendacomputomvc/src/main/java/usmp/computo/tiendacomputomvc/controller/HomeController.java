b72028b39c8a7f96eca2b9b99acfd57ef7b6395d
Fred Duarte <fduartej@gmail.com>
2021-06-12 16:43:30 -0500

    Fix Folder a name Welcome

--- tiendacomputomvc/src/main/java/usmp/computo/tiendacomputomvc/controller/HomeController.java
+++ tiendacomputomvc/src/main/java/usmp/computo/tiendacomputomvc/controller/HomeController.java
@@ -1,5 +1,5 @@
   
-package usmp.computo.tiendacomputomvc.web;
+package usmp.computo.tiendacomputomvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- tiendacomputomvc/src/main/java/usmp/computo/tiendacomputomvc/controller/UserController.java
+++ tiendacomputomvc/src/main/java/usmp/computo/tiendacomputomvc/controller/UserController.java
@@ -1,4 +1,4 @@
-package usmp.computo.tiendacomputomvc.web;
+package usmp.computo.tiendacomputomvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
