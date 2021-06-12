6d8f932eb5f2a3e036e393226b5a5e502ca4190b
Fred Duarte <fduartej@gmail.com>
2021-06-12 16:46:23 -0500

    Fix paquetes

--- tiendacomputomvc/src/main/java/usmp/computo/tiendacomputomvc/repository/ProductoRepository.java
+++ tiendacomputomvc/src/main/java/usmp/computo/tiendacomputomvc/repository/ProductoRepository.java
@@ -3,7 +3,7 @@ package usmp.computo.tiendacomputomvc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import usmp.computo.tiendacomputomvc.domain.Producto;
+import usmp.computo.tiendacomputomvc.model.Producto;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto,String>{
